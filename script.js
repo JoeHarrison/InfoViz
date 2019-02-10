bff73c3d2a23a64ba9657c5274a7c6a147811ec5
Joe Harrison <joeharrison1994@gmail.com>
2019-02-10 15:26:07 +0100

    Data in stucture

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-console.log('test')
-d3.select("body").style("background-color", "black");
-d3.csv("./data/meteo.csv").then(function(data) {
-  console.log(data[0]);
+let data_location = "https://joeharrison.github.io/InfoViz/data/meteo.csv"
+let ymdt = {};
+
+d3.csv(data_location).then(function(data) {
+    for (let key in data){
+        let value = data[key];
+
+        let year = parseInt(value["year"]);
+
+        if (isNaN(year)){
+            continue
+        }
+
+        let month = parseInt(value["month"]);
+        let day = parseInt(value["day"]);
+        let temperature = value["temperature"];
+
+        if (!ymdt[year]){
+            ymdt[year] = {};
+        }
+        if (!ymdt[year][month]){
+            ymdt[year][month] = {};
+        }
+        if (!ymdt[year][month][day]){
+            ymdt[year][month][day] = [];
+        }
+
+        ymdt[year][month][day].push(temperature);
+
+    }
 });
+
+console.log(ymdt.map((s)=>s));
+
+let svg = d3.select('svg');
+let chart = svg.append('g');
+let yScale = d3.scaleLinear().range([100,0]).domain([0,140]);
+chart.append('g').call(d3.axisLeft(yScale));
+
+let xScale = d3.scaleBand().range([0,100]).domain([])
